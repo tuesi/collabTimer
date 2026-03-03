ef70b69b4e519e12eaec7e5b493710884bf81820
Adomaitis Tadas <tadas.adomaitis@mif.stud.vu.lt>
2026-03-03 09:56:01 +0200

    added proper title

--- server/src/helpers/generateCode.ts
+++ server/src/helpers/generateCode.ts
@@ -1,6 +1,6 @@
 import { customAlphabet } from 'nanoid'
 
-const nanoid = customAlphabet('ABCDEFGHJKLMNPQRSTUVWXYZ23456789', 6)
+const nanoid = customAlphabet('ABCDEFGHJKLMNPQRSTUVWXYZ0123456789', 4)
 
 export function generateUniqueCode(activeCodes: Set<string>): string {
   let code: string
